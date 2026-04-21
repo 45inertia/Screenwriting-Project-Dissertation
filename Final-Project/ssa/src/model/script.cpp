b87bdc8174a25823fc0fce1a99fcf07700d9f18a
45inertia <5myero37@solent.ac.uk>
2026-04-21 22:05:33 +0100

    script update

--- Final-Project/ssa/src/model/script.cpp
+++ Final-Project/ssa/src/model/script.cpp
@@ -1,3 +1,20 @@
 #include "script.h"
+#include "model/scene.h"
+#include <QString>
+#include <vector>
 
 Script::Script() {}
+
+QString Script::getTitle() const { return title_; }
+
+QString Script::getAuthor() const { return author_; }
+
+const std::vector<Scene>& Script::getScenes() const { return scenes_; }
+
+void Script::setTitle(const QString newTitle) { title_ = newTitle; }
+
+void Script::setAuthor(const QString newAuthor) { author_ = newAuthor; }
+
+void Script::addScene(const Scene newScene) { scenes_.push_back(newScene); }
+
+void Script::removeScene(int sceneNumber) {}
--- Final-Project/ssa/src/model/script.h
+++ Final-Project/ssa/src/model/script.h
@@ -1,13 +1,28 @@
 #ifndef SCRIPT_H
 #define SCRIPT_H
 
-class Script
-{
-private:
+#include "model/scene.h"
+#include <QString>
+#include <vector>
 
+class Script {
+private:
+    QString title_;
+    QString author_;
+    std::vector<Scene> scenes_;
 
 public:
     Script();
+
+    // getters and setters
+    QString getTitle() const;
+    QString getAuthor() const;
+    const std::vector<Scene>& getScenes() const;
+
+    void setTitle(const QString newTitle);
+    void setAuthor(const QString newAuthor);
+    void addScene(const Scene newScene);
+    void removeScene(int sceneNumber);
 };
 
 #endif // SCRIPT_H
