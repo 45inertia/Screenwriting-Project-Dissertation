dcf481f646e1d5dadf687906d22e4f497c2372eb
45inertia <5myero37@solent.ac.uk>
2026-04-23 21:02:05 +0100

    feat(MainWindow): setups and menu actions complete.
    Setup complete for central widget, menu bar and status bar. Menu actions
    have also been completed.

--- Final-Project/ssa/src/view/mainwindow.cpp
+++ Final-Project/ssa/src/view/mainwindow.cpp
@@ -1,18 +1,19 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
-#include "view/mainwindow.h"
 #include "ui_mainwindow.h"
 #include "view/scripteditor.h"
+#include "view/scenenavigator.h"
+#include "view/newscriptdialog.h"
 #include "viewmodel/scriptviewmodel.h"
 #include "services/scriptmanager.h"
-#include "view/scenenavigator.h"
+#include "model/ElementType.h"
 
-#include <QSplitter>
-#include <QToolBar>
-#include <QStatusBar>
-#include <QLabel>
 #include <QMenuBar>
+#include <QStatusBar>
 #include <QFileDialog>
+#include <QStandardPaths>
+#include <QMessageBox>
+#include <QMenu>
+#include <QAction>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -28,6 +29,33 @@ MainWindow::MainWindow(QWidget *parent)
     scriptEditor_ = new ScriptEditor(scriptViewModel_, this);
     sceneNavigator_ = new SceneNavigator(scriptViewModel_, this);
 
+    setupCentralWidget();
+    setupMenuBar();
+    setupStatusBar();
+
+    // connecting ViewModel signals to MainWindow slots
+    connect(scriptViewModel_, &ScriptViewModel::scriptTitleChanged,
+            this, &MainWindow::onScriptTitleChanged);
+
+    connect(scriptViewModel_, &ScriptViewModel::elementTypeChanged,
+            this, &MainWindow::onElementTypeChanged);
+
+    connect(scriptViewModel_, &ScriptViewModel::currentSceneChanged,
+            this, &MainWindow::updateSceneIndicator);
+
+    // update word count whenever the document changes
+    connect(scriptEditor_->document(), &QTextDocument::contentsChanged,
+            this, &MainWindow::updateWordCount);
+
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
+// ----setup--------------------------------------------------------------------------------------
+
+void MainWindow::setupCentralWidget() {
     // splitter
     qSplitter_ = new QSplitter(Qt::Horizontal, this);
     qSplitter_->addWidget(sceneNavigator_);
@@ -38,28 +66,145 @@ MainWindow::MainWindow(QWidget *parent)
     qSplitter_->setStretchFactor(1, 4);
 
     setCentralWidget(qSplitter_);
-
     scriptEditor_->setPlaceholderText("Begin writing your screenplay...");
-
-    scriptManager_->newScript("Test Script", "Oliver Myers");
-    scriptViewModel_->onNewSceneRequested();
-
-
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
-void MainWindow::setupToolBar() {
-// TODO
 }
 
 void MainWindow::setupMenuBar() {
-// TODO
+    QMenu* fileMenu = menuBar()->addMenu("File");
+
+    QAction* newAction = fileMenu->addAction("New Script");
+    newAction->setShortcut(QKeySequence::New);
+
+    QAction* openAction = fileMenu->addAction("Open...");
+    openAction->setShortcut(QKeySequence::Open);
+
+    QAction* saveAction = fileMenu->addAction("Save");
+    saveAction->setShortcut(QKeySequence::Save);
+
+    QAction* saveAsAction = fileMenu->addAction("Save As...");
+    saveAsAction->setShortcut(QKeySequence::SaveAs);
+
+    fileMenu->addSeparator();
+
+    QAction* quitAction = fileMenu->addAction("Quit");
+    quitAction->setShortcut(QKeySequence::Quit);
+
+    // connecting the actions to the functions
+    connect(newAction, &QAction::triggered, this, &MainWindow::onNewScript);
+    connect(openAction, &QAction::triggered, this, &MainWindow::onOpenScript);
+    connect(saveAction, &QAction::triggered, this, &MainWindow::onSaveScript);
+    connect(saveAsAction, &QAction::triggered, this, &MainWindow::onSaveAsScript);
+    connect(quitAction, &QAction::triggered, this, &QApplication::quit);
 }
 
+// contains information on
+// - word count
+// - current ScriptElement
+// - current Scene
+// - page count
 void MainWindow::setupStatusBar() {
-// TODO
+    // word count
+    wordCountLabel_ = new QLabel("Words: 0", this);
+    wordCountLabel_->setMinimumWidth(100);
+    statusBar()->addWidget(wordCountLabel_);
+
+    // spacer to push element type to the centre as it is the most important and used
+    QWidget* leftSpacer = new QWidget(this);
+    leftSpacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+    statusBar()->addWidget(leftSpacer);
+
+    // element type which is central and clickable
+    elementTypeLabel_ = new QLabel("ACTION", this);
+    elementTypeLabel_->setAlignment(Qt::AlignCenter);
+    elementTypeLabel_->setMinimumWidth(160);
+    elementTypeLabel_->setStyleSheet(
+        "QLabel {"
+        "font-weight: bold;"
+        "font-family: 'Courier New';"
+        "padding: 2px 8px;"
+        "border: 1px solid palette(mid);"
+        "border-radius: 3px;"
+        "}"
+        "QLabel:hover {"
+        "background-color: palette(highlight);"
+        "color: palette(highlighted-text);"
+        "}"
+    );
+    elementTypeLabel_->setCursor(Qt::PointingHandCursor);
+    elementTypeLabel_->installEventFilter(this);
+    statusBar()->addWidget(elementTypeLabel_);
+
+    // spacer to push scene and page to the right
+    QWidget* rightSpacer = new QWidget(this);
+    rightSpacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+    statusBar()->addWidget(rightSpacer);
+
+    // Scene indicator
+    sceneIndicatorLabel_ = new QLabel("Scene1", this);
+    sceneIndicatorLabel_->setMinimumWidth(80);
+    statusBar()->addPermanentWidget(sceneIndicatorLabel_);
+
+    //page count
+    pageCountLabel_ = new QLabel("Page 1", this);
+    pageCountLabel_->setMinimumWidth(60);
+    statusBar()->addPermanentWidget(pageCountLabel_);
+
+}
+
+
+// Event Filter for element type label click------------------------------------------------------
+
+//bool MainWindow::eventFilter(QObject* obj, QEvent* event) {
+
+//}
+
+
+// ---- menu actions -----------------------------------------------------------------------------
+
+void MainWindow::onNewScript() {
+    NewScriptDialog dialog(this);
+    if(dialog.exec() == QDialog::Accepted) {
+        currentFilePath_ = dialog.getFilePath();
+        scriptViewModel_->onNewScriptRequested(dialog.getTitle(), dialog.getAuthor());
+
+        // saving immediately to the chosen location
+        scriptViewModel_->onSaveRequested(currentFilePath_);
+        setWindowTitle("SSA - " + dialog.getTitle());
+    }
+}
+
+void MainWindow::onOpenScript() {
+    QString path = QFileDialog::getOpenFileName(
+        this,
+        "Open Script",
+        QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation),
+        "Open Screenplay Format (*.osf)"
+    );
+
+    if(!path.isEmpty()) {
+        currentFilePath_ = path;
+        scriptViewModel_->onLoadRequested(path);
+    }
+}
+
+void MainWindow::onSaveScript() {
+    if(currentFilePath_.isEmpty()) {
+        onSaveAsScript();
+        return;
+    }
+    scriptViewModel_->onSaveRequested(currentFilePath_);
+}
+
+void MainWindow::onSaveAsScript() {
+    QString path = QFileDialog::getSaveFileName(
+        this,
+        "Save Script As",
+        QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation),
+        "Open Screenplay Format (*.osf)"
+    );
+
+    if(!path.isEmpty()) {
+        currentFilePath_ = path;
+        scriptViewModel_->onSaveRequested(path);
+    }
 }
--- Final-Project/ssa/src/view/mainwindow.h
+++ Final-Project/ssa/src/view/mainwindow.h
@@ -12,6 +12,16 @@ class ScriptNavigator;
 class QSplitter;
 class QLabel;
 
+#include <QMainWindow>
+#include <QLabel>
+#include <QSplitter>
+
+// forward declarations
+class ScriptEditor;
+class SceneNavigator;
+class ScriptViewModel;
+class ScriptManager;
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -28,24 +38,38 @@ public:
 
 private slots:
     void onNewScript();
-
+    void onOpenScript();
+    void onSaveScript();
+    void onSaveAsScript();
+    void onScriptTitleChanged(const QString& title);
+    void onElementTypeChanged(ElementType type);
+    void onStatusElementTypeClicked();
+    void updateWordCount();
+    void updateSceneIndicator(int sceneIndex);
 
 private:
     Ui::MainWindow *ui;
-    ScriptEditor* scriptEditor_;
-    ScriptViewModel* scriptViewModel_;
+
+    // core components
     ScriptManager* scriptManager_;
+    ScriptViewModel* scriptViewModel_;
+    ScriptEditor* scriptEditor_;
     SceneNavigator* sceneNavigator_;
     QSplitter* qSplitter_;
+
+    // status bar labels
+    QLabel* wordCountLabel_;
     QLabel* elementTypeLabel_;
+    QLabel* sceneIndicatorLabel_;
     QLabel* pageCountLabel_;
 
+    // Current save path
+    QString currentFilePath_;
 
-    void setupCentralWidget();
-    void setupToolBar();
     void setupMenuBar();
     void setupStatusBar();
-
+    void setupCentralWidget();
+    QString elementTypeToString(ElementType type) const;
 
 };
 #endif // MAINWINDOW_H
